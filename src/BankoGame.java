7f0e1e6810793ae8d9b02e3a83a4f58847a59f86
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-01-10 11:18:39 +0100

    Completed number generator for bankogame with checks and list

--- src/BankoGame.java
+++ src/BankoGame.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class BankoGame implements CheckingForWins{
     public static void main(String[] args) {
-
+        BankoGame bg = new BankoGame();
         BankoBoardGenerator bbg = new BankoBoardGenerator();
         ArrayList<Integer> bankoBoards = new ArrayList<>(bbg.bankNumbersGenerator(50));
         ArrayList<Boolean> bankoBoardCheck = new ArrayList<>(Collections.nCopies(bankoBoards.size(), false));
@@ -17,9 +17,8 @@ public class BankoGame implements CheckingForWins{
         while(!success){
             try {
                 System.out.println("Press [1] to draw a new number. Press [2] to check a banko board.");
-                String select = sc.next();
-                Integer.parseInt(select);
-                switch (Integer.parseInt(select)) {
+                int select = Integer.parseInt(sc.next());
+                switch (select) {
                     case 1:
                         boolean success2 = false;
                         while(!success2) {
@@ -39,6 +38,14 @@ public class BankoGame implements CheckingForWins{
                         }
                         System.out.println(bankoBoardCheck);
                     case 2:
+                        System.out.println("Enter caller ID.");
+                        int callerID = Integer.parseInt(sc.next());
+                        boolean winner = bg.checkBooleans(callerID, bankoBoardCheck);
+                        if(winner){
+                            System.out.println(callerID + " is a winner.");
+                        }
+                    case 3:
+                        success = true;
                 }
                 }catch(NumberFormatException NFE){
                     System.out.println("That is not a number.");
@@ -46,13 +53,48 @@ public class BankoGame implements CheckingForWins{
         }
     }
     @Override
-    public boolean checkBooleans(ArrayList<Boolean> list) {
-        return true;
+    public boolean checkBooleans(int callerID, ArrayList<Boolean> list) {
+        System.out.println("Check for how many numbers of rows [1], [2], [3]");
+        Scanner sc = new Scanner(System.in);
+        boolean success = false;
+        boolean winner = false;
+        int plateID = callerID*15;
+        boolean[] plateNumbers = new boolean[15];
+        for(int i = plateID; i < plateID+15; i++){
+            plateNumbers[i] = list.get(i);
+        }
+        while(!success) {
+            try {
+                int select = Integer.parseInt(sc.next());
+                switch(select){
+                    case 1:
+                        for(int i = 0; i <= plateNumbers.length; i = i+5){
+                            for(int j = 0; j < i+5; j++){
+                                if(!plateNumbers[i]){
+                                    winner = true;
+                                    break;
+                                }
+                            }
+                        }
+                        success = true;
+                    case 2:
+
+                        success = true;
+                    case 3:
+
+                        success = true;
+                }
+
+            } catch (NumberFormatException NFE) {
+                System.out.println("That is not a number.");
+            }
+        }
+        return winner;
     }
 }
 /*When the game begins everyone has empty plates.
 The Caller randomly draws a number and tells it loud and clear.
-Every player, with that number on a plate(s), cover the number with
+Every player, with that number on a plate(s), covers the number with
 a plastic chip.
 The Caller draws a new number and continues to repeat that until
 one (or eventually more) player(s) claim “BANKO” for the first row
--- src/CheckingForWins.java
+++ src/CheckingForWins.java
@@ -1,5 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public interface CheckingForWins {
-    public boolean checkBooleans(ArrayList<Boolean> list);
+    public boolean checkBooleans(int callerID, ArrayList<Boolean> list);
 }
