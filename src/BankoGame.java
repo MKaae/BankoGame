f52065f684ced5ac2a399c7ac6d1f32b3ad7beff
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-01-10 10:35:22 +0100

    Completed number generator for bankogame with checks and list

--- src/BankoGame.java
+++ src/BankoGame.java
@@ -1,33 +1,53 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
-public class BankoGame {
+public class BankoGame implements CheckingForWins{
     public static void main(String[] args) {
 
         BankoBoardGenerator bbg = new BankoBoardGenerator();
         ArrayList<Integer> bankoBoards = new ArrayList<>(bbg.bankNumbersGenerator(50));
         ArrayList<Boolean> bankoBoardCheck = new ArrayList<>(Collections.nCopies(bankoBoards.size(), false));
+        ArrayList<Integer> drawnNumbers = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
+        Random r = new Random();
         boolean success = false;
 
         while(!success){
-            try{
-                String entry = sc.next();
-                Integer.parseInt(entry);
-
-                if(bankoBoards.contains(Integer.parseInt(entry))){
-                    for(int i = 0; i < bankoBoards.size(); i++){
-                        if(bankoBoards.get(i).equals(Integer.parseInt(entry))){
-                            bankoBoardCheck.set(i, true);
+            try {
+                System.out.println("Press [1] to draw a new number. Press [2] to check a banko board.");
+                String select = sc.next();
+                Integer.parseInt(select);
+                switch (Integer.parseInt(select)) {
+                    case 1:
+                        boolean success2 = false;
+                        while(!success2) {
+                            int entry = r.nextInt(90 - 1) + 1;
+                            if (drawnNumbers.size() == 0 || !drawnNumbers.contains(entry)) {
+                                drawnNumbers.add(entry);
+                                System.out.println(entry);
+                                if (bankoBoards.contains(entry)) {
+                                    for (int i = 0; i < bankoBoards.size(); i++) {
+                                        if (bankoBoards.get(i).equals(entry)) {
+                                            bankoBoardCheck.set(i, true);
+                                        }
+                                    }
+                                }
+                                success2 = true;
+                            }
                         }
-                    }
+                        System.out.println(bankoBoardCheck);
+                    case 2:
                 }
-
-            }catch(NumberFormatException NFE){
-                System.out.println("That is not a number.");
-            }
+                }catch(NumberFormatException NFE){
+                    System.out.println("That is not a number.");
+                }
         }
+    }
+    @Override
+    public boolean checkBooleans(ArrayList<Boolean> list) {
+        return true;
     }
 }
 /*When the game begins everyone has empty plates.
---
+++ src/CheckingForWins.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public interface CheckingForWins {
+    public boolean checkBooleans(ArrayList<Boolean> list);
+}
